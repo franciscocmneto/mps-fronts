b7f226ecfc60012384d480c842bde4495ff6e9a0
Francisco Morais <fdcmn.snf20@uea.edu.br>
2022-05-03 11:21:52 -0400

    added tela2

--- front-tela2/src/pages/App.tsx
+++ front-tela2/src/pages/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Formulario from '../components/formulario'
 import Lista from '../components/lista';
+import EnhancedTable from '../components/tabela';
 import './style.scss'
 
 function App() {
   return (
     <div className="AppStyle">
-      <Formulario />
-      <Lista />
+      <EnhancedTable/>
     </div>
   );
 }
